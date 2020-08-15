74ebd5c1210954558aa801c581ab9cdb24afa9f7
Myat-Min-Khant <backuser1112@gmail.com>
2020-08-16 00:55:07 +0630

    First Commit

---
+++ build_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:51c62fc1a3360fe923526a1ee4e66a5774dc965357dbf6241fce81e64ac53add
+size 1887
---
+++ live_webcam.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:08072479bba88ad9a081630ea05b5a30e428b44947f9dc4a1547d1a1a9b333f9
+size 3092
